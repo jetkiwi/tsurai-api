ca6937af9b63834917982b8b083b572ed5bf3842
ja731j <jetkiwi@gmail.com>
2016-10-30 11:23:38 +0900

    Bind to 0.0.0.0

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ import os
 if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.add_api('swagger.yaml', arguments={'title': 'Share your Tsurai'})
-    app.run(port=os.environ.get('PORT',8080))
+    app.run(host='0.0.0.0',port=os.environ.get('PORT',8080))
